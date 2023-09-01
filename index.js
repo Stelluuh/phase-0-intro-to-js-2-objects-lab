f764a8883eb576b71959aed37b08d6259e4cdbb0
stella <sftamesis13@gmail.com>
2023-09-01 11:44:40 -0700

    passes first 3 tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+const employee = {
+    name: "Stella",
+    streetAddress: "123 my address"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value
+        return employee
+}
